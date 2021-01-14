76f742b2908c29808a9a1ca6195724c15ffea50e
Anurag Gupta <guptaanurag1610@gmail.com>
2021-01-14 12:52:51 +0530

    Indian currency

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -7,7 +7,7 @@ export const Transaction = ({ transaction }) => {
     <li className={transaction.amount < 0 ? "minus" : "plus"}>
       {transaction.text}{" "}
       <span>
-        {sign}${Math.abs(transaction.amount)}
+        {sign}Rs.{Math.abs(transaction.amount)}
       </span>
       <button
         onClick={() => deleteTransaction(transaction.id)}
